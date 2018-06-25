614884fec97fa1e9af1a838b51cfd9f7e2fa4fb6
Junyuan Zheng <jzheng@Junyuans-MacBook-Pro.local>
2018-06-25 14:41:22 -0700

    Add Question 171
    Add Question 171

---
+++ Question171.cpp
@@ -0,0 +1,13 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int titleToNumber(string s) {
+        int result = 0;
+        for (char c : s)
+            result = result * 26 + (c - 'A' + 1);
+        return result;
+    }
+};
