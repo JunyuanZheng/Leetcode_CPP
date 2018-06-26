cf5c029dba1cc774461258d3fedfc5557a2ecd1f
Junyuan Zheng <jzheng@Junyuans-MacBook-Pro.local>
2018-06-26 09:07:30 -0700

    Add Question 215
    Add Question 215

---
+++ Question215.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for (int i : nums) {
+            pq.push(i);
+            if (pq.size() > k) {
+                pq.pop();
+            }
+        }
+        return pq.top();
+    }
+};
