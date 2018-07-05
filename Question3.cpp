396e88330b1d65bb727fd29bfc274010ffd31557
Junyuan Zheng <jzheng@Junyuans-MacBook-Pro.local>
2018-07-05 10:21:02 -0700

    Update Question 3
    Update Question 3

--- Question3.cpp
+++ Question3.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <string>
 #include <vector>
 
@@ -7,22 +6,15 @@ using namespace std;
 class Solution {
 public:
     static int lengthOfLongestSubstring(string s) {
-        int left = 0;
-        int result = 0;
-        vector<int> dict(256, -1);
-        for (int i = 0; i < s.length(); i += 1) {
+        int size = (int) s.size(), result = 0;
+        vector<int> v (128, -1);
+        for (int i = 0, j = 0; i < size; i += 1) {
             char c = s[i];
-            if (dict[c] != -1) {
-                left = max(dict[c] + 1, left);
-            }
-            dict[c] = i;
-            result = max(i - left + 1, result);
+            if (v[c] != -1)
+                j = max(v[c] + 1, j);
+            v[c] = i;
+            result = max(result, i - j + 1);
         }
         return result;
     }
 };
-
-int main()
-{
-    cout << Solution::lengthOfLongestSubstring("abcabcbb");
-}
