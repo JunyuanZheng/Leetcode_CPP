9683b0344bc1c414cf224ea9f72cb71ddf52d453
Junyuan Zheng <jzheng@Junyuans-MacBook-Pro.local>
2018-07-06 20:46:15 -0700

    Update Question 66
    Update Question 66

--- Question66.cpp
+++ Question66.cpp
@@ -5,17 +5,19 @@ using namespace std;
 class Solution {
 public:
     vector<int> plusOne(vector<int> &digits) {
-        int overflow = 0;
+        int overflow = 1;
         for (int i = (int) digits.size() - 1; i >= 0; i -= 1) {
-            if (digits[i] < 9) {
-                digits[i] += 1;
+            if (digits[i] + overflow <= 9) {
+                digits[i] += overflow;
                 return digits;
             } else {
-                digits[i] = 0;
+                int tmp = digits[i];
+                digits[i] = (digits[i] + overflow) % 10;
+                overflow = (tmp + overflow) / 10;
             }
         }
         vector<int> result (digits.size() + 1, 0);
-        result[0] = 1;
+        result[0] = overflow;
         return result;
     }
 };
