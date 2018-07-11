165f39168e9997e8f83b72af41c2ca30bda1ad26
Junyuan Zheng <jzheng@Junyuans-MacBook-Pro.local>
2018-07-10 21:59:29 -0700

    Update Question 118
    Update Question 118

--- Question118.cpp
+++ Question118.cpp
@@ -7,11 +7,11 @@ public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> result;
         for (int i = 1; i <= numRows; i += 1) {
-            vector<int> v ((unsigned long)i, 1);
-            for (int j = 1; j < i - 1; j += 1) {
-                v[j] = result[result.size() - 1][j - 1] + result[result.size() - 1][j];
+            vector<int> tmp (static_cast<unsigned long>(i), 1);
+            for (int j = 1; j <= i - 2; j += 1) {
+                tmp[j] = result[i - 2][j - 1] + result[i - 2][j];
             }
-            result.push_back(v);
+            result.push_back(tmp);
         }
         return result;
     }
