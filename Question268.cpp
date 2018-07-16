1c7c7d0ab38b3bbef6aa64691255d0ceeb535d34
Junyuan Zheng <joseph.zjy@gmail.com>
2018-07-16 15:35:39 -0700

    Update Question 268
    Update Question 268

--- Question268.cpp
+++ Question268.cpp
@@ -5,9 +5,9 @@ using namespace std;
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int result = (int) nums.size();
-        for (int i = 0; i < nums.size(); i += 1) {
-            result = result ^ i ^ nums[i];
+        int size = (int) nums.size(), result = size;
+        for (int i = 0; i < size; i += 1) {
+            result ^= (i ^ nums[i]);
         }
         return result;
     }
